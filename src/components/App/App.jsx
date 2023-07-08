ff54fea279b35234fc0ada48c8ad3148ae65f523
Pavlo <teenitclub@gmail.com>
2023-07-08 12:58:36 +0300

    First Structur

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../Header/Header";
+import FrontPage from "../FrontPage/FrontPage";
+import Footer from "../Footer/Footer";
+import User from "../User/User";
+
+const App = ()=>{
+    return(
+        <div>
+            <Header />
+            <FrontPage />
+            <Footer />
+            <User />
+        </div>
+    )
+}
+export default App;
---
+++ src/components/FrontPage/FrontPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const FrontPage = ()=>{
+    return(
+        <div>Front Page</div>
+    )
+}
+export default FrontPage;
