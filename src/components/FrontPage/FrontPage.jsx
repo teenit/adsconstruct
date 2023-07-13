38900a38e738e2a9bcdd08c757255d3a6db1022b
kleema <vovanklim2006@gmail.com>
2023-07-13 19:35:43 +0300

    mainBlock+header

--- src/components/FrontPage/FrontPage.jsx
+++ src/components/FrontPage/FrontPage.jsx
@@ -10,11 +10,80 @@ const FrontPage = ()=>{
 
     return(
         <div className={s.wrap}>
-            <div className={s.wrap__96}>
-                <div className={s.module}>
-                    <button onClick={()=>setState({...state,modal:true})}>Открыть окно</button>
-                </div>
+            <div className={s.module}>
+                   <div className={s.blocks1}>
+                   <div className={s.block}></div>
+                   </div>
+                   </div>
+                   <div className={s.module}>
+                   <div className={s.blocks2}></div>
+                   <div className={s.block}>  </div>
+                   <div className={s.block}></div>
+                 
+                   </div>
+                   <div className={s.module}>
+                   <div className={s.blocks3}></div>
+                   <div className={s.block}>  </div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+
+                   </div>
+
+                   <div className={s.module}>
+                   <div className={s.blocks4}>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   
+                   </div>
             </div>
+                   
+                   <div className={s.module}>
+                   <div className={s.blocks5}></div>
+                   <div className={s.block}>  </div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                 
+                   </div>
+                   <div className={s.module}>
+                   <div className={s.blocks6}></div>
+                   <div className={s.block}>  </div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                 
+                   </div>
+                   <div className={s.module}>
+                   <div className={s.blocks7}></div>
+                   <div className={s.block}>  </div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                 
+                   </div>
+
+                <div className={s.module}>
+                   <div className={s.blocks8}>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   <div className={s.block}></div>
+                   </div>
+                
+            </div>
+          
             {state.modal ?<ProfileNavModule close = {()=>setState({...state,modal:false})} />:null} 
         </div>
     )
