9b70169b36f171d9336c2efcf1f119eab6109e98
Pavlo <teenitclub@gmail.com>
2023-10-07 14:32:48 +0300

    Pavlo 12

--- src/components/Pages/FrontPage/FrontPage.jsx
+++ src/components/Pages/FrontPage/FrontPage.jsx
@@ -83,12 +83,70 @@ const FrontPage = () => {
             }
             ]
         },
+        {
+            userName: "name",
+            userNickname: "id",
+            buyTime: "12.09.2023 13:44",
+            property: {
+                defend: true,
+                defendTo: "13.09.2023 19:44",
+            },
+            id: "8",
+            type: 4,
+            position: 8,
+            blocks: [{
+                userName: "",
+                userNickname: "",
+                id: "",
+                buyTime: "12.09.2023 13:44",
+                property: {
+                    defend: true,
+                    defendTo: "13.09.2023 19:44",
+                }, 
+                position: 0,
+                idSection: "",
+                elements: [{
+                    element:"h1",
+                    data:"uhefbwe fw efiwe fiwe",
+                    id:"",
+                    idBlock:"",
+                    position:0,
+                    attributes:{
+                        style:{color:"red"},
+                        width:"100%"
+                    }
+                },
+                {
+                    element:"p",
+                    data:"uhefbwe fw efiwe fiwe <h2>BIG TEXT</h2> nekjfw fwefj wehjf hew",
+                    id:"",
+                    idBlock:"",
+                    position:0,
+                    attributes:{
+                        style:{color:"green",fontSize:"10px"},
+                        width:"100%"
+                    }
+                }]
+            }, {
+                userName: "",
+                userNickname: "",
+                id: "",
+                buyTime: "12.09.2023 13:44",
+                property: {
+                    defend: true,
+                    defendTo: "13.09.2023 19:44",
+                }, position: 1,
+                idSection: "",
+                elements:[]
+            },
+            ]
+        },
 
     ]
     return (
         <div className={s.wrap}>
-            <Sections data={mas[0]} />
-            <Sections data={mas[1]} />
+            <Sections data={mas[2]} />
+           
             {mas[0].blocks[0].elements.map((item, index) => {
                 const styles = Object.keys(item.attributes.style).map(key => `${key}:${item.attributes[key]}`)
                 const attributes = Object.keys(item.attributes).map(key => `${key}=${item.attributes[key]}`)
--- src/components/Sections/Block.jsx
+++ src/components/Sections/Block.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { createElement, useEffect, useState } from 'react'
 import s from './Block.module.css'
 import plus from '../../img/add.png'
 import BlockModal from './BlockModal'
@@ -8,10 +8,46 @@ const Block = ({buy,data}) => {
   const [hover, setHover] = useState(false)
   const [modal, setModal] = useState(false)
   const { blocks } = useSelector(state => state.sections)
+  console.log(data)
+  return(
+    <div className={`${s.block} ${buy?'bought':'not-bought'}`}>
+      {
+        !buy &&(
+          <div className={s.hover}>
+            <img src={plus} className={s.img__plus} alt="Купить блок" onClick={() => {
+              setModal(true)
+              console.log(blocks);
+            }} />
+          </div>
+        )
+      }
+      {
+        buy &&(
+          <div className={s.block__content}>
+            {
+              data.elements.map((item)=>{
+                return createElement(item.element,item.attributes,item.data
+                  )
+                
+              })
+            }
+          </div>
+        )
+      }
+      <div className={s.content__block}>
+
+      </div>
+      {
+        modal&&(
+          <BlockModal setModal={setModal} />
+        )
+      }
+    </div>
+  )
   return (
     <>
       <div className={s.block} onMouseEnter={() => { setHover(true) }} onMouseLeave={() => { setHover(false) }}>
-        {hover&&buy ? <div className={s.hover}>
+        {hover&&buy === false ? <div className={s.hover}>
           <img src={plus} alt="Купить блок" onClick={() => {
             setModal(true)
             console.log(blocks);
